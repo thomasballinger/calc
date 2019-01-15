5bfe7d26a170193e72d72e39679658ce7d46226c
Thomas Ballinger <thomasballinger@gmail.com>
2019-01-14 22:19:45 -0800

    add unary ops and greater and less than

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from num2words import num2words
 
 class Token(namedtuple('Token', ['kind', 'content'])):
     @staticmethod
@@ -15,6 +16,13 @@ class Token(namedtuple('Token', ['kind', 'content'])):
         elif s == '/': return Token('Slash', s)
         elif s == '(': return Token('Left Paren', s)
         elif s == ')': return Token('Right Paren', s)
+        elif s == '>': return Token('Greater', s)
+        elif s == '<': return Token('Less', s)
+        elif s == '=': return Token('Equals', s)
+        elif s == 'p': return Token('Print', s)
+        elif s == 's': return Token('String', s)
+        elif s == 'l': return Token('Length', s)
+        elif s in ('x', 'y', 'z'): return Token('Variable', s)
         elif s.isnumeric(): return Token('Number', int(s))
         else: raise ValueError("Can't parse string '{}' into token".format(s))
 
@@ -31,7 +39,7 @@ def tokenize(string):
                 tokens.append(Token.from_string(token_string))
                 token_string = ''
             token_string = ''
-        elif c in ('+', '-', '*', '/', '(', ')'):
+        elif c in ('+', '-', '*', '/', '(', ')', '>', '<', '=', 'p', 's', 'l', 'x', 'y', 'z'):
             working_on_a_number = False
             if token_string:
                 tokens.append(Token.from_string(token_string))
@@ -45,21 +53,35 @@ def tokenize(string):
 
     return tokens
 
-BinOp = namedtuple('BinOp', ['left', 'op', 'right'])
+BinaryOp = namedtuple('BinaryOp', ['left', 'op', 'right'])
+UnaryOp = namedtuple('UnaryOp', ['op', 'right'])
 
 def pprint_tree(node, indent=2):
     if isinstance(node, Token):
         print(' '*indent + str(node.content))
-    else:
+    elif isinstance(node, BinaryOp):
         print(' '*indent + node.op.content)
         pprint_tree(node.left, indent + 2)
         pprint_tree(node.right, indent + 2)
+    elif isinstance(node, UnaryOp):
+        print(' '*indent + node.op.content)
+        pprint_tree(node.right, indent + 2)
 
 def parse(tokens):
-    tree, unused_tokens = parse_plus_or_minus(tokens)
+    tree, unused_tokens = parse_greater_or_less(tokens)
     if unused_tokens:
         raise ValueError("Trailing unparsed tokens: {}".format(unused_tokens))
     return tree
+
+def parse_greater_or_less(tokens):
+    expr, remaining_tokens = parse_plus_or_minus(tokens)
+
+    if remaining_tokens and remaining_tokens[0].kind in ('Greater', 'Less'):
+        op, *remaining_tokens = remaining_tokens
+        right, remaining_tokens = parse_plus_or_minus(remaining_tokens)
+        expr = BinaryOp(expr, op, right)
+
+    return expr, remaining_tokens
 
 def parse_plus_or_minus(tokens):
     expr, remaining_tokens = parse_multiply_or_divide(tokens)
@@ -67,7 +89,7 @@ def parse_plus_or_minus(tokens):
     while remaining_tokens and remaining_tokens[0].kind in ('Plus', 'Minus'):
         op, *remaining_tokens = remaining_tokens
         right, remaining_tokens = parse_multiply_or_divide(remaining_tokens)
-        expr = BinOp(expr, op, right)
+        expr = BinaryOp(expr, op, right)
 
     return expr, remaining_tokens
 
@@ -77,7 +99,15 @@ def parse_multiply_or_divide(tokens):
     while remaining_tokens and remaining_tokens[0].kind in ('Star', 'Slash'):
         op, *remaining_tokens = remaining_tokens
         right, remaining_tokens = parse_operand(remaining_tokens)
-        expr = BinOp(expr, op, right)
+        expr = BinaryOp(expr, op, right)
+
+    return expr, remaining_tokens
+
+def parse_unary_op(tokens):
+    op, *remaining_tokens = tokens
+    assert op.kind in ('Plus', 'Minus', 'Print', 'String', 'Length')
+    operand, remaining_tokens = parse_operand(remaining_tokens)
+    expr = UnaryOp(op=op, right=operand)
 
     return expr, remaining_tokens
 
@@ -85,9 +115,12 @@ def parse_operand(tokens):
     if tokens[0].kind == 'Number':
         expr, *remaining_tokens = tokens
         return expr, remaining_tokens
+    elif tokens[0].kind in ('Minus', 'Plus', 'Print', 'String', 'Length'):
+        expr, remaining_tokens = parse_unary_op(tokens)
+        return expr, remaining_tokens
     elif tokens[0].kind == 'Left Paren':
         _, *remaining_tokens = tokens
-        expr, remaining_tokens = parse_plus_or_minus(remaining_tokens)
+        expr, remaining_tokens = parse_greater_or_less(remaining_tokens)
         right_paren, *remaining_tokens = remaining_tokens
         if not right_paren.kind == 'Right Paren':
             raise ValueError('Expected {} to be a right paren'.format(right_paren))
@@ -95,18 +128,29 @@ def parse_operand(tokens):
     else:
         raise ValueError("Can't parse tokens: {}".format(tokens))
 
-funcs = {
+binary_funcs = {
     'Plus': lambda x, y: x+y,
     'Minus': lambda x, y: x-y,
     'Star': lambda x, y: x*y,
     'Slash': lambda x, y: x/y,
+    'Greater': lambda x, y: x > y,
+    'Less': lambda x, y: x < y,
+}
+unary_funcs = {
+    'Plus': lambda x: x,
+    'Minus': lambda x: -x,
+    'Print': lambda x: (print(x), x)[1],
+    'String': num2words,
+    'Length': len,
 }
 
 def evaluate(node):
     if isinstance(node, Token):
         return node.content
-    else:
-        return funcs[node.op.kind](evaluate(node.left), evaluate(node.right))
+    elif isinstance(node, BinaryOp):
+        return binary_funcs[node.op.kind](evaluate(node.left), evaluate(node.right))
+    elif isinstance(node, UnaryOp):
+        return unary_funcs[node.op.kind](evaluate(node.right))
 
 def debug_repl():
     while True:

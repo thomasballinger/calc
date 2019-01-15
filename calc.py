470c6aac211b9f7d7662ed1a3d48dbb3ed324029
Thomas Ballinger <thomasballinger@gmail.com>
2019-01-15 12:42:42 -0800

    type inference

--- calc.py
+++ calc.py
@@ -18,13 +18,42 @@ unary_funcs = {
     'String': num2words,
     'Length': len,
 }
+signatures = {
+    ('Plus', int, int): int,
+    ('Plus', str, str): str,
+    ('Print', str): str,
+    ('Print', int): int,
+    ('Minus', int, int): int,
+    ('Minus', int): int,
+    ('Plus', int): int,
+    ('Star', int, int): int,
+    ('Slash', int, int): int,
+    ('Greater', int, int): bool,
+    ('Less', int, int): bool,
+    ('String', int): str,
+}
 
 def execute(stmt):
     if isinstance(stmt, (BinaryOp, UnaryOp, Token)):
+        print(type_infer(stmt))
         value = evaluate(stmt)
     elif isinstance(stmt, Assignment):
         value = evaluate(stmt.expression)
         print('set', stmt.variable.content, 'to', value)
+
+def type_infer(node):
+    if isinstance(node, Token):
+        if node.kind == 'Number': return int
+    elif isinstance(node, BinaryOp):
+        left_type = type_infer(node.left)
+        right_type = type_infer(node.right)
+        result_type = signatures[(node.op.kind, left_type, right_type)]
+        return result_type
+    elif isinstance(node, UnaryOp):
+        expression_type = type_infer(node.right)
+        result_type = signatures[(node.op.kind, expression_type)]
+        return result_type
+
 
 def evaluate(node):
     if isinstance(node, Token):
